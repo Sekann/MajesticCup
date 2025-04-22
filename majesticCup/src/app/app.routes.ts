8a7abf4c66b9e4161837da5a5e5110e335203333
Your Name <you@example.com>
2025-04-22 15:30:03 +0200

    inicio y header sin completar

--- majesticCup/src/app/app.routes.ts
+++ majesticCup/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { InicioComponent } from './inicio/inicio.component';
+import { DraftComponent } from './draft/draft.component';
+import { EstadisticasComponent } from './estadisticas/estadisticas.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: InicioComponent },
+    { path: 'estadisticas', component: EstadisticasComponent },
+    { path: 'draft', component: DraftComponent }
+];
