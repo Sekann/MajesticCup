fb8615b1e2579b97fa15efec36582ed7d26a8dd9
drodriguez5503 <davidrdelope@gmail.com>
2025-04-22 16:15:28 +0200

    Primeros cambios y diseño inicial draft component

--- majesticCup/src/app/app.routes.ts
+++ majesticCup/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import {DraftComponent} from './draft/draft.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path:'draft', component: DraftComponent}
+];
--- majesticCup/src/app/draft/draft.component.ts
+++ majesticCup/src/app/draft/draft.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import {NgForOf} from '@angular/common';
 
 @Component({
   selector: 'app-draft',
-  imports: [],
+  imports: [
+    NgForOf
+  ],
   templateUrl: './draft.component.html',
   styleUrl: './draft.component.scss'
 })
 export class DraftComponent {
+  equipo1 = new Array(5);
+  equipo2 = new Array(5);
 
 }
