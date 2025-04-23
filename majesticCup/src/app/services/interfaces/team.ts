62666906c94cb9cade2277ca9acda85b3b00850c
Your Name <you@example.com>
2025-04-23 16:29:39 +0200

    falta responsive y lo de back

--- majesticCup/src/app/services/interfaces/team.ts
+++ majesticCup/src/app/services/interfaces/team.ts
@@ -1,7 +1,10 @@
+import { Player } from "./player";
+
 export interface Team {
   slug: string;
   name: string;
   victories: number;
   losses: number;
   winrate: number;
+  players: Player[];
 }
