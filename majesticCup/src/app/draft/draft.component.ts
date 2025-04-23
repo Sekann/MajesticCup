f0af7e486edaddd12218f0659e0cfce77246d4b0
drodriguez5503 <davidrdelope@gmail.com>
2025-04-23 16:28:53 +0200

    Prueba baneo heroes hecha

--- majesticCup/src/app/draft/draft.component.ts
+++ majesticCup/src/app/draft/draft.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import {NgForOf} from '@angular/common';
+import {HeroInterface} from '../services/interfaces/hero-interface';
 
 @Component({
   selector: 'app-draft',
@@ -10,7 +11,54 @@ import {NgForOf} from '@angular/common';
   styleUrl: './draft.component.scss'
 })
 export class DraftComponent {
-  team1 = new Array(5);
-  team2 = new Array(5);
+  bannedHeroes: HeroInterface[] = [];
+  firstBanned= new Array<HeroInterface>(5);
+  secondBanned = new Array<HeroInterface>(5);
 
+
+  getBannedHeroes() {
+    this.bannedHeroes = [
+    {slug: "ffdfdf",
+      name:"Heroe1",
+      image:"fdjfdfdjfdjf"},
+      {slug: "ffdfdf",
+        name:"Heroe2",
+        image:"fdjfdfdjfdjf"},
+      {slug: "ffdfdf",
+        name:"Heroe3",
+        image:"fdjfdfdjfdjf"},
+      {slug: "ffdfdf",
+        name:"Heroe4",
+        image:"fdjfdfdjfdjf"},
+      {slug: "ffdfdf",
+        name:"Heroe5",
+        image:"fdjfdfdjfdjf"},
+      {slug: "ffdfdf",
+        name:"Heroe6",
+        image:"fdjfdfdjfdjf"},
+      {slug: "ffdfdf",
+        name:"Heroe7",
+        image:"fdjfdfdjfdjf"},
+      {slug: "ffdfdf",
+        name:"Heroe8",
+        image:"fdjfdfdjfdjf"},
+      {slug: "ffdfdf",
+        name:"Heroe9",
+        image:"fdjfdfdjfdjf"},
+      {slug: "ffdfdf",
+        name:"Heroe10",
+        image:"fdjfdfdjfdjf"},
+
+    ]
+
+    for (let i = 0; i < 5; i++) {
+      this.firstBanned[i] = this.bannedHeroes[i];
+      this.secondBanned[i] = this.bannedHeroes[i+5];
+    }
+
+    console.log(this.bannedHeroes);
+    console.log(this.firstBanned);
+    console.log(this.secondBanned);
+
+  }
 }
